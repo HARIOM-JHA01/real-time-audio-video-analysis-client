33a1e9e72ec0d495e8481daaaa1aa4ba8e6038a9
HARIOM-JHA01 <jhahariom040@gmail.com>
2025-09-18 18:09:18 +0530

    fix: ensure basePath is correctly set in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/real-time-audio-video-analysis/client',
 };
 
 export default nextConfig;
