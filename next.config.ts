e4fed89c7287b07e0fd660cb5f9738517a9c3f4c
HARIOM-JHA01 <jhahariom040@gmail.com>
2025-09-18 18:12:46 +0530

    fix: remove basePath configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: '/real-time-audio-video-analysis/client',
 };
 
 export default nextConfig;
